e3b1404f04f07811eddf333d1df1fb3139c814ef
bhavnamulani <bhavana.mulani@gmail.com>
2017-05-10 18:49:44 +0530

    implementation of chat

---
+++ Vchat 3/Vchat/MessagesDo.swift
@@ -0,0 +1,25 @@
+//
+//  MessagesDo.swift
+//  Vchat
+//
+//  Created by Ranjit singh on 5/9/17.
+//  Copyright © 2017 ranjit singh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MessagesDo {
+    
+    var msgText: String?
+    var sender: String?
+    var sentBy = LGChatMessage.SentBy.Opponent
+
+    
+//    init (msgText: String, sender: String) {
+//        self.msgText = msgText
+//        print("Msg from do",self.msgText)
+//        self.sender = sender
+//        print("type from do",self.sender)
+//
+//    }
+}
